fa5959c3db8277a8988aadc1875cbcc5a4cb0bd0
miles.chou <miles.chou@104.com.tw>
2025-06-18 15:32:06 +0800

    Fix for 8.4 deprecation

--- src/Http/Message/Traits/UploadedFileFactoryDetector.php
+++ src/Http/Message/Traits/UploadedFileFactoryDetector.php
@@ -49,10 +49,10 @@ trait UploadedFileFactoryDetector
      */
     public function createUploadedFile(
         StreamInterface $stream,
-        int $size = null,
+        ?int $size = null,
         int $error = \UPLOAD_ERR_OK,
-        string $clientFilename = null,
-        string $clientMediaType = null
+        ?string $clientFilename = null,
+        ?string $clientMediaType = null
     ): UploadedFileInterface {
         return $this->uploadedFileFactory()->createUploadedFile(
             $stream,
